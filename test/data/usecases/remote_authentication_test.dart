7ebb0e3d8d6025db3af1f9875a5450d09db9b7ba
aadelino <adelino.mescouto.ramos@gmail.com>
2021-03-21 12:25:26 -0300

    verificação de url passada correta

---
+++ test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,42 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:meta/meta.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({
+    @required this.httpClient,
+    @required this.url,
+  });
+
+  Future<void> auth() async {
+    await httpClient.request(url: url);
+  }
+}
+
+abstract class HttpClient {
+  Future<void> request({
+    @required String url,
+  });
+}
+
+class HttpClientSpy extends Mock implements HttpClient {
+  
+}
+
+void main() {
+  test("should call httpClient with correct url", () async {
+    final httpClient = HttpClientSpy();
+    final url = faker.internet.httpUrl();
+    final systemUniteTest =
+        RemoteAuthentication(httpClient: httpClient, url: url);
+
+    await systemUniteTest.auth();
+
+    verify(httpClient.request(url: url));
+  });
+}
