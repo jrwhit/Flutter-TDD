b319a04586f41206ff34370d93dead2ab55bf0bf
aadelino <adelino.mescouto.ramos@gmail.com>
2021-03-21 20:45:39 -0300

    error 500

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -73,4 +73,22 @@ void main() {
       expect(future, throwsA(DomainError.unexpected));
     },
   );
+
+  test(
+    "should throw UnexpectedError if httpclient return 500",
+        () async {
+      when(
+        httpClient.request(
+          url: anyNamed('url'),
+          method: anyNamed('method'),
+          body: anyNamed("body"),
+        ),
+      ).thenThrow(HttpError.serverError);
+
+      final future = systemUniteTest.auth(params);
+
+      expect(future, throwsA(DomainError.unexpected));
+    },
+  );
+
 }
