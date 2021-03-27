6224a40206d7376fec72cf0b7ec817609baf7ea5
aadelino <adelino.mescouto.ramos@gmail.com>
2021-03-26 23:22:20 -0300

    mock para casos de sucesso e falha

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -6,23 +6,28 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-class HttpAdapter {
+import 'package:for_dev/data/http/http_client.dart';
+
+class HttpAdapter implements HttpClient {
   Map<String, dynamic> body;
 
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<Map<String, dynamic>> request({
     @required String url,
     @required String method,
     Map<String, dynamic> body,
   }) async {
     final headers = {
       'content-type': 'application/json',
-      'accept': 'aplication/json',
+      'accept': 'application/json',
     };
-    await client.post(url, headers: headers, body: body != null ? jsonEncode(body) : null);
+    final response = await client.post(url,
+        headers: headers, body: body != null ? jsonEncode(body) : null);
+
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -39,33 +44,74 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
-  group("post", () {
-    test('should call post with correct value', () async {
-      await systemUniteTest
-          .request(url: url, method: "post", body: {'any_key': 'any_value'});
+  group(
+    "post",
+    () {
+      PostExpectation mockRequest() => when(client.post(any,
+          body: anyNamed('body'), headers: anyNamed('headers')));
 
-      verify(
-        client.post(url,
-            headers: {
-              'content-type': 'application/json',
-              'accept': 'aplication/json',
-            },
-            body: '{"any_key":"any_value"}'),
+      void mockResponse(int statusCode,
+          {String body = '{"any_key":"any_value"}'}) {
+        mockRequest().thenAnswer((_) async => Response(body, statusCode));
+      }
+
+      setUp(() {
+        mockResponse(200);
+      });
+      test(
+        'should call post with correct value',
+        () async {
+          await systemUniteTest.request(
+              url: url, method: "post", body: {'any_key': 'any_value'});
+
+          verify(
+            client.post(
+              url,
+              headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json',
+              },
+              body: '{"any_key":"any_value"}',
+            ),
+          );
+        },
       );
-    });
 
-    test('should call post without body', () async {
-      await systemUniteTest.request(url: url, method: "post");
+      test(
+        'should call post without body',
+        () async {
+          await systemUniteTest.request(
+            url: url,
+            method: "post",
+          );
 
-      verify(
-        client.post(
-          url,
-          headers: {
-            'content-type': 'application/json',
-            'accept': 'aplication/json',
-          },
-        ),
+          verify(
+            client.post(any, headers: anyNamed('headers')),
+          );
+        },
       );
-    });
-  });
+
+      test(
+        'should call return data if post return 200',
+        () async {
+          final response =
+              await systemUniteTest.request(url: url, method: "post");
+
+          expect(response, {'any_key': 'any_value'});
+        },
+      );
+
+      test(
+        'should call return null if post return 200 with no data',
+        () async {
+          mockResponse(200, body: '');
+
+          final response =
+              await systemUniteTest.request(url: url, method: "post");
+
+          expect(response, null);
+        },
+      );
+    },
+  );
 }
